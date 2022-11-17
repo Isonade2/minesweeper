4dbfc8e2a470e0bc95446333e52e8bbaed4a90f5
Isonade2 <c1004sos@naver.com>
2022-11-18 00:27:26 +0900

    12331

---
+++ 니편내편/Test/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SCORE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "<BLUE>\r\n<GREEN>\r\n<PURPLE>\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "<RED>\r\n<PINK>\r\n<PURPLE>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "방향키로 조작";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 407);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(398, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(422, 442);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ 니편내편/니편내편/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 니편내편
+{
+    public partial class Form1 : Form
+    {
+        Bitmap red = Properties.Resources.red;
+        Bitmap blue = Properties.Resources.blue;
+        Bitmap green = Properties.Resources.green;
+        Bitmap pink = Properties.Resources.pink;
+        Bitmap brown = Properties.Resources.brown;
+        Bitmap purple = Properties.Resources.purple;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+        Random rnd = new Random();
+        int number = 2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(number);
+            }
+            if (arr[0] == 0)
+                pictureBox1.Image = red;
+            else
+                pictureBox1.Image = blue;
+            if (arr[1] == 0)
+                pictureBox2.Image = red;
+            else
+                pictureBox2.Image = imageList1.Images[1];
+            if (arr[2] == 0)
+                pictureBox3.Image = imageList1.Images[0];
+            else
+                pictureBox3.Image = imageList1.Images[1];
+            if (arr[3] == 0)
+                pictureBox4.Image = imageList1.Images[0];
+            else
+                pictureBox4.Image = imageList1.Images[1];
+            if (arr[4] == 0)
+                pictureBox5.Image = imageList1.Images[0];
+            else
+                pictureBox5.Image = imageList1.Images[1];
+            
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                try
+                {
+                    Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                    Color color = bitmap.GetPixel(e.X, e.Y);
+                    label1.Text = $"RED : {color.R} GREEN : {color.G} BLUE : {color.B}";
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+          
+            if (e.KeyCode == Keys.Left)
+            {
+
+                if (true)
+                {
+                    pictureBox1.Image = pictureBox2.Image;
+                    pictureBox2.Image = pictureBox3.Image;
+                    pictureBox3.Image = pictureBox4.Image;
+                    pictureBox4.Image = pictureBox5.Image;
+
+                    makebuttoncolor();
+
+
+                }
+                else
+                {
+                    Application.Restart();
+                    MessageBox.Show("gameover");
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (pictureBox1.Image.Size == imageList1.Images[0].Size)
+                {
+                    pictureBox1.Image = pictureBox2.Image;
+                    pictureBox2.Image = pictureBox3.Image;
+                    pictureBox3.Image = pictureBox4.Image;
+                    pictureBox4.Image = pictureBox5.Image;
+
+                    makebuttoncolor();
+                }
+                else
+                {
+                    Application.Restart();
+                    MessageBox.Show("gameover");
+                }
+            }
+            if (e.KeyCode == Keys.F1)
+            {
+                MessageBox.Show("Hi");
+            }
+        }
+        private void makebuttoncolor()
+        {
+
+            int a = rnd.Next(number);
+            pictureBox5.Image = imageList1.Images[a];
+            pictureBox5.Image = imageList1.Images[a];
+
+        }
+    }
+}
